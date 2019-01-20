73bb3057cce91939eccaf7e1c1c9eb7f27218938
kimse9450@gmail.com <kimse9450@gmail.com>
2019-01-20 18:04:09 +0900

    Create boj.problem2909 KMP는 왜 KMP일까?

---
+++ src/main/java/boj/problem2909/Main.java
@@ -0,0 +1,19 @@
+package boj.problem2909;
+
+/**
+ * https://www.acmicpc.net/problem/2902
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = scanner.nextLine().split("-");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            sb.append(strings[i].charAt(0));
+        }
+        System.out.println(sb);
+    }
+}
