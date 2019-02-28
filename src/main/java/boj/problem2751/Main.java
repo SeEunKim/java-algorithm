f6d4a04306439692a100d846627b1c0a34e20878
kimse9450@gmail.com <kimse9450@gmail.com>
2019-02-28 15:54:59 +0900

    Create boj 2751

---
+++ src/main/java/boj/problem2751/Main.java
@@ -0,0 +1,23 @@
+package boj.problem2751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
