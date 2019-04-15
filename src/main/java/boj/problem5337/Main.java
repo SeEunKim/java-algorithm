f00712e4efa18f024296dda2061a4028fc332919
kimse9450@gmail.com <kimse9450@gmail.com>
2019-04-15 14:17:24 +0900

    Create boj problem5337

---
+++ src/main/java/boj/problem5337/Main.java
@@ -0,0 +1,14 @@
+package boj.problem5337;
+
+/**
+ * Created by SeEun Kim.
+ * Date: 2019-04-15
+ * Time: 14:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
